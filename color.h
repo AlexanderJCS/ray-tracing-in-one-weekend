11c5f2c660c7a5192f987f43b8d1976295037f4c
Alex <alexandercastronovo@gmail.com>
2024-11-30 18:37:06 -0500

    chapter 9.5

--- color.h
+++ color.h
@@ -6,12 +6,20 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component) {
+    if (linear_component > 0) {
+        return std::sqrt(linear_component);
+    }
+
+    return 0;
+}
+
 void write_color(std::ostream& out, const color& pixel_color) {
     static const interval intensity(0, 0.999);
 
-    out << int(256 * intensity.clamp(pixel_color.x())) << ' '
-        << int(256 * intensity.clamp(pixel_color.y())) << ' '
-        << int(256 * intensity.clamp(pixel_color.z())) << '\n';
+    out << int(256 * intensity.clamp(linear_to_gamma(pixel_color.x()))) << ' '
+        << int(256 * intensity.clamp(linear_to_gamma(pixel_color.y()))) << ' '
+        << int(256 * intensity.clamp(linear_to_gamma(pixel_color.z()))) << '\n';
 }
 
 #endif  // RAY_TRACER_COLOR_H
