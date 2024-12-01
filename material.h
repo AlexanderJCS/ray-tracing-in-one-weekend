8a536581de2c2778792061aec1951bb3aaee0f83
Alex <alexandercastronovo@gmail.com>
2024-11-30 21:55:12 -0500

    chapter 11.5

--- material.h
+++ material.h
@@ -55,4 +55,39 @@ private:
     double fuzz;
 };
 
+class dielectric : public material {
+public:
+    explicit dielectric(double refraction_index) : refraction_index(refraction_index) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+        attenuation = color(1, 1, 1);
+        double ri = rec.front_face ? (1 / refraction_index) : refraction_index;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1);
+        double sin_theta = std::sqrt(1 - cos_theta * cos_theta);
+
+        bool cannot_refract = ri * sin_theta > 1;
+        vec3 direction;
+
+        if (cannot_refract || reflectance(cos_theta, ri) > random_double()) {
+            direction = reflect(unit_direction, rec.normal);
+        } else {
+            direction = refract(unit_direction, rec.normal, ri);
+        }
+
+        scattered = ray(rec.p, direction);
+        return true;
+    }
+
+private:
+    double refraction_index;
+
+    static double reflectance(double cosine, double refraction_index) {
+        // Using Schlick's approximation for reflectance
+        double r0 = std::pow((1 - refraction_index) / (1 + refraction_index), 2);
+        return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+    }
+};
+
 #endif  // RAY_TRACER_MATERIAL_H
